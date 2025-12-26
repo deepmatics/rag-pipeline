59331882ffb3839da459add1490faa9d1d3fcd92
deepmatics <satish.epgm21@mdpalumni.iimcal.ac.in>
2025-12-26 12:39:08 +0530

    updated the repo to uv rather than pip

---
+++ experiments/hf download.py
@@ -0,0 +1,9 @@
+#download open rag dataset
+from huggingface_hub import snapshot_download
+
+# This explicitly defines it as a dataset
+path = snapshot_download(
+    repo_id="vectara/open_ragbench", 
+    repo_type="dataset"
+)
+print(f"Downloaded to: {path}")ß
